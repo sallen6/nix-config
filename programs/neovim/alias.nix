874b02f98ffc836425ec35144209cd451fa0b201
sallen6 <s.allen@elsevier.com>
2025-07-24 10:57:59 -0400

    Changes to shell aliases

--- programs/neovim/alias.nix
+++ programs/neovim/alias.nix
@@ -1,11 +1,20 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   # Neovim specific shell aliases
   programs.zsh = {
     shellAliases = {
+      # GNU Utils
+      cat = "bat -p"; 
+      ls = "eza";
+      rm = "rm -I";
+
+      # Editor
       nv = "nvim";
       nvfzf = "nvim $(fzf)";
+
+      # Misc
+      weather = "curl wttr.in/Philadelphia";
     };
   };
 }
